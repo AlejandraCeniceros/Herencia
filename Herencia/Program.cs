ef7d8bd9735707371045432fa4156ea74bea6727
alumno <alumno@E302>
2019-09-09 12:38:27 -0700

    090919

--- Herencia/Program.cs
+++ Herencia/Program.cs
@@ -15,6 +15,30 @@ namespace Herencia
             triangulo.Color = "Amarillo";
             Console.WriteLine(triangulo.toString());
 
+            triangulo.Altura = 2.5;
+            triangulo.Base = 4;
+
+            double areaTriangulo = triangulo.calcularArea();
+            Console.WriteLine("Area de triangulo" + areaTriangulo);
+
+            Circulo circulo = new Circulo(5.4);
+            Console.WriteLine("Area del Cirulo" + circulo.calcularArea().ToString());
+
+            Figura triangulo2 = new Triangulo(); // polimorfismo triangulo comportandose como figura
+            ((Triangulo)triangulo2).Base = 2;
+            ((Triangulo)triangulo2).Altura = 3;
+            Console.WriteLine("Area de triangulo 2" + triangulo2.calcularArea().ToString());
+
+            List<Figura> figuras = new List<Figura>(); // polimorfismo lista con distintos tipos de datos
+            figuras.Add(triangulo);
+            figuras.Add(circulo);
+            figuras.Add(triangulo2);
+
+            foreach ( var figura in figuras)
+            {
+                Console.WriteLine("Area : " + figura.calcularArea().ToString());
+            }
+
 
             Console.Read();
         }
